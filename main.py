45101a496089fcdef625ce0847443d2944b00aac
Shru Kumar <shrutika.skumar@gmail.com>
2024-02-19 20:38:31 -0500

    mongodb implemented

---
+++ main.py
@@ -0,0 +1,46 @@
+"""
+author: Shru Kumar
+filename: main.py
+description: utilizes MongoAPI and answers 10 queries
+"""
+import pymongo
+from mongo_api import MongoAPI
+from pprint import pprint
+
+mongo = MongoAPI(db = 'Cooking', col = 'Recipes')
+
+# Q1: Can I get a list of meals that fulfill my desired macronutrient goals?
+macro_meals = mongo.meals_with_macros(protein = 30, calories = 300, sodium = 400, fat = 20, num = 20)
+# pprint(macro_meals)
+
+# Q2: Which recipes have 3 ingredients or less?
+max_ingr_recipes = mongo.count_ingredients(max_ingredients=3, num=20)
+# pprint(max_ingr_recipes)
+
+# Q3: What is the average rating of all recipes
+avg_rating = mongo.average_tag(tag = 'rating')
+# print(avg_rating)
+
+# Q4: What is the correlation between sodium and calories in all recipes? (vis1) (scatterplot)
+# mongo.scatter('sodium', 'calories')
+
+# Q5: Which 10 categories are there the most recipes for? (vis2) (bar graph)
+# mongo.count_categories(num = 10)
+
+# Q6: Which categories have the highest ratings? (vis3) (bar graph)
+# mongo.category_ratings(num = 20)
+
+# Q7: Can I look for recipes with a specific ingredient?
+# recipes = mongo.search_ingredient('bacon', num = 15)
+# pprint(recipes)
+
+# Q8: What is the historic trend for recipe uploads? (vis4) (line graph)
+# mongo.timeline()
+
+# Q9: How many distinct-categories are there?
+distinct = mongo.distinct_tag('categories')
+# print(distinct)
+
+# Q10: Which recipes have the fewest directions?
+directions = mongo.few_directions(num = 20)
+# pprint(directions)
---
+++ mongo_api.py
@@ -0,0 +1,220 @@
+"""
+author: Shru Kumar
+filename: mongo_api.py
+description: establishing class MongoAPI and functions to answer 10 queries
+"""
+import pymongo
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+class MongoAPI:
+
+    def __init__(self, db, col, client = "mongodb://localhost:27017/"):
+        self.myclient = pymongo.MongoClient(client)
+        self.mydb = self.myclient[db]
+        self.mycol = self.mydb[col]
+
+    # Q1
+    def meals_with_macros(self, protein, calories, sodium, fat, num = 10):
+        """Takes desired macro breakdown and returns list of matching recipes
+        Args:
+            protein (int): desired grams of protein threshold
+            calories (int): desired calories threshold
+            sodium (int): desired grams of sodium threshold
+            fat (int): desired grams of fat threshold
+        Returns:
+            result (list): list of recipes that meet criteria and associated macros
+        """
+        query = {"protein": {"$gte": protein}, 'calories': {"$lte": calories}, 'sodium': {"$lte": sodium}, 
+                 'fat': {"$lte": fat}}
+        projection = {"title": 1, "_id": 0, 'desc': 1, 'directions': 1, 'protein': 1, 'calories': 1, 
+                      'sodium': 1, 'fat': 1, 'rating': 1, 'ingredients': 1}
+        result = list(self.mycol.find(query, projection))[:num]
+
+        return result
+    
+    # Q2
+    def count_ingredients(self, max_ingredients = 5, num = 10):
+        """Returns recipe list including recipes with a max number of ingredients
+        Args:
+            max_ingredients (int): max desired number of ingredients 
+            num (int): number of recipes to return
+        Returns:
+            result (list): list of recipes with desired number of ingredients or less
+        """
+        pipeline = [{'$project': {'_id': 0, 'title': 1, 'num_ingredients': {'$size': '$ingredients'}, 'ingredients': 1, 
+                                  'directions': 1}},
+                    {'$match': {'num_ingredients':{'$lte': max_ingredients}}}]
+        result = list(self.mycol.aggregate(pipeline))[:num]
+
+        return result
+
+    # Q3
+    def average_tag(self, tag):
+        """Calculates average value for a specified tag
+        Args:
+            tag (str): desired tag to find average value
+        Returns:
+            result (list): list only containing average value of tag category
+        """
+        pipeline = [{'$group': {'_id':'average ' + tag, 'avg_val':{'$avg':'$' + tag}}}]
+        result = list(self.mycol.aggregate(pipeline))
+
+        return result
+
+    # Q4
+    def scatter(self, tag1, tag2):
+        """Produces scatterplot comparing two tag categories
+        Args:
+            tag1 (str): tag to be x-axis values
+            tag2 (str): tag to be y-axis values
+        """
+        projection = {tag1: 1, tag2: 1, '_id': 0}
+        result = list(self.mycol.find({}, projection)) 
+        
+        # collecting x and ys from result list
+        x = []
+        y = []
+        for idx in range(len(result)):
+            # ensuring values are present
+            if result[idx][tag1] is None:
+                pass
+            elif result[idx][tag2] is not None:
+                x.append(result[idx][tag1])
+                y.append(result[idx][tag2])
+
+        # plotting
+        fig = plt.figure(figsize=(8, 5))
+        ax = sns.scatterplot(x = x, y = y)
+        plt.xlabel(tag1)
+        plt.ylabel(tag2)
+        plt.title(tag1 + ' vs. ' + tag2)
+        plt.ticklabel_format(style='plain', axis='y')
+        plt.ticklabel_format(style='plain', axis='x')
+        plt.show()
+    
+    # Q5
+    def count_categories(self, num):
+        """Plot the categories with the most occurences
+        Args:
+            num (int): number of categories to return
+        """
+        pipeline = [{'$unwind': '$categories'},
+                    {'$group' : { '_id' : '$categories', 'count' : {'$sum' : 1}}},
+                    {"$sort" : { "count" : -1 }}]
+        result = list(self.mycol.aggregate(pipeline))[:num]
+
+        # collecting bar plot values from result list
+        counts = []
+        categories = []
+        for idx in range(len(result)):
+            categories.append(result[idx]['_id'])
+            counts.append(result[idx]['count'])
+
+        # plotting
+        fig = plt.figure(figsize=(5, 8))
+        ax = sns.barplot(x = counts, y = categories)
+        plt.xlabel('Number of Recipes')
+        plt.ylabel('Recipe Categories')
+        plt.title(f'Top {num} Categories with the Most Recipes')
+        plt.show()
+        
+    def category_ratings(self, num = 10):
+        """Plot the categories with the top average ratings
+        Args:
+            num (int): number of categories to return
+        """
+        # first pipeline for filtering categories with less than 10 occurences
+        pipeline1 = [{'$unwind': '$categories'},
+                    {'$group' : { '_id' : '$categories', 'count' : {'$sum' : 1}}}, 
+                    {"$sort" : { "count" : -1 }}, 
+                    {'$match' : {"count" :{'$gte': 10}}}
+                    ]
+        result1 = list(self.mycol.aggregate(pipeline1))
+        categories = [result1[idx]['_id'] for idx in range(len(result1))]
+
+        # second pipeline for getting top average rating
+        pipeline2 = [{'$unwind': '$categories'},
+                    {'$match' : {'categories' : {"$in": categories}}},
+                    {'$group' : { '_id' : '$categories', 'avg_rating' : {'$avg' : '$rating'}}}, 
+                    {"$sort" : { "avg_rating" : -1 }}]
+        result2 = list(self.mycol.aggregate(pipeline2))[:num]
+
+        # collecting bar plot values from result list
+        counts = []
+        categories = []
+        for idx in range(len(result2)):
+            categories.append(result2[idx]['_id'])
+            counts.append(result2[idx]['avg_rating'])
+
+        # plotting
+        fig = plt.figure(figsize=(5, 8))
+        ax = sns.barplot(x = counts, y = categories)
+        plt.xlabel('Rating')
+        plt.ylabel('Recipe Categories')
+        plt.title(f'Top {num} Categories with the Most Recipes')
+        plt.show()
+
+    # Q7    
+    def search_ingredient(self, target_ingredient, num=10):
+        """Returns list of recipes containing target ingredient
+        Args:
+            target_ingredient (str): desired ingredient
+            num (int): number of recipes to return
+        Returns:
+            result (list): list of recipes containing target ingredient
+        """
+        query = {"ingredients" : {'$regex' : target_ingredient}}
+        projection = {"title": 1, "_id": 0, 'desc': 1, 'directions': 1, 'rating': 1, 'ingredients': 1}
+        result = list(self.mycol.find(query, projection))[:num]
+
+        return result
+    
+    # Q8
+    def timeline(self):
+        """Plots number of recipes posted per year
+        """
+        projection = {"_id": 0, 'date' : 1}
+        result = list(self.mycol.find({}, projection))
+
+        dates = {}
+        for idx in range(len(result)):
+            date = result[idx]['date'][:4]
+            if date not in dates.keys():
+                dates[date] = 1
+            else:
+                dates[date] += 1
+        dates = dict(sorted(dates.items()))
+
+        fig = plt.figure(figsize=(5, 8))
+        ax = sns.lineplot(x = dates.keys(), y = dates.values())
+
+        plt.xlabel('Years')
+        plt.ylabel('Number of Recipes Published')
+        plt.title('Number of Recipes Published Over Time')
+        plt.show()
+    
+    # Q9
+    def distinct_tag(self, tag):
+        """Returns distinct values of desired tag
+        Args:
+            tag
+        """
+        result = len(list(self.mycol.distinct(tag)))
+
+        return result
+    
+    # Q10
+    def few_directions(self, num = 10):
+        """Returns recipes with the least directions
+        Args:
+            num (int): number of recipes to return
+        Returns:
+            result (list): list of recipes with the least directions
+        """
+        pipeline = [{'$unwind': '$directions'},
+                    {'$group' : { '_id' : '$title', 'count' : {'$sum' : 1}}}, 
+                    {"$sort" : { "count" : 1 }}]
+        result = list(self.mycol.aggregate(pipeline))[:num]
+
+        return result
